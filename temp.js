a3ef7bf55a28f6a61994caaaa0db404bf3139520
Chris Snowden <cdsnowden@gmail.com>
2017-07-31 11:51:10 +0100

    Init an overflow warning icon

--- temp.js
+++ temp.js
@@ -6,7 +6,27 @@ for (var i=0; i < il; i++) {
 	if (itemsall[i].offsetWidth > vw) {
     	console.log(itemsall[i]);
 		itemsall[i].setAttribute("style", "border: 5px solid red"); 
+		var topOffset = itemsall[i].getBoundingClientRect().top;
+		addOverflowWarning(itemsall[i],topOffset);
 	}
 };
 
 // add indicator - red triangle? Make sure to kill it after click off.
+function addOverflowWarning(parent,offset) {
+	var overflowWarning = document.createElement('span');
+        overflowWarning.className= "overflowWarning";
+
+        overflowWarning.style.position = "fixed";
+        overflowWarning.style.top = offset;
+        overflowWarning.style.right = "0";
+	
+	overflowWarning.style.height = "0";
+	overflowWarning.style.width = "0";
+	overflowWarning.style.zIndex = "999";
+	
+	overflowWarning.style.borderTop = "20px solid transparent";
+	overflowWarning.style.borderLeft = "40px solid red";
+	overflowWarning.style.borderBottom = "20px solid transparent";
+		
+	parent.appendChild(overflowWarning);
+}
